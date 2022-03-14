856f691f713738766b354e30548c011d1d66116b
DESKTOP-VL5REEG\quang <quangnhat180799@gmail.com>
2022-03-14 09:31:57 +0700

    first commit

---
+++ app/src/main/java/com/example/bai3la/SecondActivity.java
@@ -0,0 +1,14 @@
+package com.example.bai3la;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
